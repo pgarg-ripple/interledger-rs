61d1acdfaf786e9afd56c106d677dcf0c8449906
Georgios Konstantopoulos <me@gakonst.com>
2019-06-18 20:37:33 +0300

    implement tests for all branches (except unreachable ones)

--- crates/interledger-settlement/src/message_service.rs
+++ crates/interledger-settlement/src/message_service.rs
@@ -151,3 +151,285 @@ where
         Box::new(self.next.handle_request(request))
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    // use futures::future::ok;
+    use interledger_packet::{Fulfill, PrepareBuilder, Reject};
+    // use interledger_service::outgoing_service_fn;
+    use crate::SettlementEngineDetails;
+    use interledger_service::{incoming_service_fn, Account, BoxedIlpFuture, IncomingService};
+    use std::time::SystemTime;
+
+    use mockito::mock;
+    use mockito::Matcher;
+
+    use std::str::FromStr;
+    use tokio::runtime::Runtime;
+    use url::Url;
+
+    use std::str;
+
+    #[test]
+    fn settlement_ok_valid_json() {
+        // happy case
+        let m = mock_settle(200).create();
+        let mut settlement = test_service();
+        let destination = TEST_ACCOUNT_0.clone().ilp_address;
+        let fulfill: Fulfill = block_on(
+            settlement.handle_request(IncomingRequest {
+                from: TEST_ACCOUNT_0.clone(),
+                prepare: PrepareBuilder {
+                    amount: 0,
+                    expires_at: SystemTime::now(),
+                    destination,
+                    data: VALID_JSON.as_bytes(),
+                    execution_condition: &[0; 32],
+                }
+                .build(),
+            }),
+        )
+        .unwrap();
+
+        m.assert();
+        assert_eq!(fulfill.data(), BODY.as_bytes());
+        assert_eq!(fulfill.fulfillment(), &[0; 32]);
+    }
+
+    #[test]
+    fn settlement_ok_invalid_json() {
+        // the request is rejected so the api should never be hit
+        let m = mock_settle(200).create().expect(0);
+        let mut settlement = test_service();
+        let destination = TEST_ACCOUNT_0.clone().ilp_address;
+        let reject: Reject = block_on(
+            settlement.handle_request(IncomingRequest {
+                from: TEST_ACCOUNT_0.clone(),
+                prepare: PrepareBuilder {
+                    amount: 0,
+                    expires_at: SystemTime::now(),
+                    destination,
+                    data: INVALID_JSON.as_bytes(),
+                    execution_condition: &[0; 32],
+                }
+                .build(),
+            }),
+        )
+        .unwrap_err();
+
+        m.assert();
+        assert_eq!(reject.code(), ErrorCode::F00_BAD_REQUEST);
+        assert_eq!(reject.triggered_by(), SERVICE_ADDRESS.clone());
+        assert_eq!(
+            reject.message(),
+            "Unable to parse message as JSON: Error(\"expected value\", line: 1, column: 1)"
+                .as_bytes()
+        );
+    }
+
+    #[test]
+    fn no_settlement_engine_configured_for_destination() {
+        // happy case
+        let m = mock_settle(200).create().expect(0);
+        let mut settlement = test_service();
+        let destination = Address::from_str("example.some.address").unwrap();
+        let reject: Reject = block_on(
+            settlement.handle_request(IncomingRequest {
+                from: TEST_ACCOUNT_0.clone(),
+                prepare: PrepareBuilder {
+                    amount: 0,
+                    expires_at: SystemTime::now(),
+                    destination,
+                    data: VALID_JSON.as_bytes(),
+                    execution_condition: &[0; 32],
+                }
+                .build(),
+            }),
+        )
+        .unwrap_err();
+
+        m.assert();
+        assert_eq!(reject.code(), ErrorCode::F02_UNREACHABLE);
+        assert_eq!(reject.triggered_by(), SERVICE_ADDRESS.clone());
+        assert_eq!(
+            reject.message(),
+            "Got settlement packet from account 0 but there is no settlement engine url configured for it".as_bytes(),
+        );
+    }
+
+    #[test]
+    fn account_does_not_have_settlement_engine() {
+        // happy case
+        let m = mock_settle(200).create().expect(0);
+        let mut settlement = test_service();
+        let mut acc = TEST_ACCOUNT_0.clone();
+        acc.no_details = true; // Hide the settlement engine data from the account
+        let reject: Reject = block_on(
+            settlement.handle_request(IncomingRequest {
+                from: acc.clone(),
+                prepare: PrepareBuilder {
+                    amount: 0,
+                    expires_at: SystemTime::now(),
+                    destination: acc.ilp_address,
+                    data: VALID_JSON.as_bytes(),
+                    execution_condition: &[0; 32],
+                }
+                .build(),
+            }),
+        )
+        .unwrap_err();
+
+        m.assert();
+        assert_eq!(reject.code(), ErrorCode::F02_UNREACHABLE);
+        assert_eq!(reject.triggered_by(), SERVICE_ADDRESS.clone());
+        assert_eq!(
+            reject.message(),
+            "No other incoming handler!".as_bytes(),
+        );
+    }
+
+    #[test]
+    fn settlement_engine_rejects() {
+        // for whatever reason the engine rejects our request with a 500 code
+        let error_code = 500;
+        let error_str = "Internal Server Error";
+        let m = mock_settle(error_code).create();
+        let destination = TEST_ACCOUNT_0.clone().ilp_address;
+        let mut settlement = test_service();
+        let reject: Reject = block_on(
+            settlement.handle_request(IncomingRequest {
+                from: TEST_ACCOUNT_0.clone(),
+                prepare: PrepareBuilder {
+                    amount: 0,
+                    expires_at: SystemTime::now(),
+                    destination: destination.clone(),
+                    data: VALID_JSON.as_bytes(),
+                    execution_condition: &[0; 32],
+                }
+                .build(),
+            }),
+        )
+        .unwrap_err();
+
+        m.assert();
+        assert_eq!(reject.code(), ErrorCode::T00_INTERNAL_ERROR);
+        // The engine rejected the message, not the connector's service, 
+        // so the triggered by should be the ilp address of th engine - I think.
+        assert_eq!(reject.triggered_by(), destination); 
+        assert_eq!(
+            reject.message(),
+            format!("Settlement engine rejected request with error code: {} {}", error_code, error_str).as_bytes(),
+        );
+    }
+
+    ////////// HELPERS (should they go to their own file? //////////
+
+    impl TestAccount {
+        pub fn new(id: u64, url: &str, ilp_address: &str) -> Self {
+            Self {
+                id,
+                url: Url::parse(url).unwrap(),
+                ilp_address: Address::from_str(ilp_address).unwrap(),
+                no_details: false,
+            }
+        }
+    }
+
+    // todo: replace with actually valid data
+    static VALID_JSON: &str = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    static INVALID_JSON: &str = "asdf";
+    static BODY: &str = "hi";
+
+    lazy_static! {
+        static ref TEST_MUTEX: Mutex<()> = Mutex::new(());
+        static ref TEST_ACCOUNT_0: TestAccount =
+            TestAccount::new(0, "http://localhost:1234", "peer.settle.xrp-ledger");
+        static ref SERVICE_ADDRESS: Address = Address::from_str("example.connector").unwrap();
+        static ref SETTLEMENT_API: Matcher =
+            Matcher::Regex(r"^/accounts/\d*/settlement$".to_string());
+    }
+
+    fn mock_settle(status_code: usize) -> mockito::Mock {
+        mock("POST", SETTLEMENT_API.clone())
+            .with_status(status_code)
+            .with_body(BODY)
+    }
+
+
+    // Futures helper taken from the store_helpers in interledger-store-redis.
+    pub fn block_on<F>(f: F) -> Result<F::Item, F::Error>
+    where
+        F: Future + Send + 'static,
+        F::Item: Send,
+        F::Error: Send,
+    {
+        // Only run one test at a time
+        let _ = env_logger::try_init();
+        let lock = TEST_MUTEX.lock();
+        let mut runtime = Runtime::new().unwrap();
+        let result = runtime.block_on(f);
+        drop(lock);
+        result
+    }
+
+    fn test_service() -> SettlementMessageService<
+        impl IncomingService<TestAccount, Future = BoxedIlpFuture> + Clone,
+        TestAccount,
+    > {
+        SettlementMessageService::new(
+            SERVICE_ADDRESS.clone(),
+            incoming_service_fn(|_request| {
+                Box::new(err(RejectBuilder {
+                    code: ErrorCode::F02_UNREACHABLE,
+                    message: b"No other incoming handler!",
+                    data: &[],
+                    triggered_by: Some(&SERVICE_ADDRESS),
+                }
+                .build()))
+            }),
+        )
+    }
+
+    /// PROBABLY COULD USE SOME UNIVERSAL TEST HELPERS FOR SERVICES SINCE THE
+    /// TESTING STYLE IS VERY SIMILAR?
+
+    #[derive(Debug, Clone)]
+    struct TestAccount {
+        pub id: u64,
+        pub url: Url,
+        pub ilp_address: Address,
+        pub no_details: bool,
+    }
+
+    impl Account for TestAccount {
+        type AccountId = u64;
+
+        fn id(&self) -> u64 {
+            self.id
+        }
+    }
+    impl SettlementAccount for TestAccount {
+        fn settlement_engine_details(&self) -> Option<SettlementEngineDetails> {
+            if self.no_details {
+                return None;
+            }
+            Some(SettlementEngineDetails {
+                url: self.url.clone(),
+                ilp_address: self.ilp_address.clone(),
+                asset_scale: 9,
+            })
+        }
+    }
+
+}
